e5a46be4e9e2d4051a407a5b9c8a41ac9fbd3011
rylan-mccluskey <rylan.mccluskey@edu.sait.ca>
2025-01-21 15:43:48 -0700

    Studentinfo

---
+++ app/week-2/student-info.js
@@ -0,0 +1,6 @@
+class StudentInfo extends React.Component {
+    render() {
+        return<h2>Rylan McCluskey</h2>
+        return<a>https://github.com/rylan-mccluskey/cprg306-assignments</a>
+    }
+}
