619916e1cbe1c137b4ae990fa34c14921a75eace
rylan-mccluskey <rylan.mccluskey@edu.sait.ca>
2025-02-04 14:52:12 -0700

    Commit and complicated, repeated code on item-list

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,3 +1,8 @@
-export default function Item() {
-    
-}
+export function Item({ name, quantity, category }) {
+    return (
+      <li className="mb-4 indent-7">
+        <p className="font-bold text-xl">{name}</p>
+        <p>Buy {quantity} in {category}</p>
+      </li>
+    );
+  }
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -1,10 +1,11 @@
-import ItemList from "./item-list"
+import ItemList from "./item-list";
 
 export default function Page() {
-    return (
-      <main>
-        <h1>Shopping List</h1>
-        <ItemList />
-      </main>
-    );
-  }
+  return (
+    <main>
+      <h1 className="font-bold text-3xl">Shopping List</h1>
+      <br></br>
+      <ItemList />
+    </main>
+  );
+}
